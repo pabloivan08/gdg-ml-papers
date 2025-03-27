2a995079bf90e1c5427cc169de7850bb89a609ef
KillerTurkey <pablo.ivan08@gmail.com>
2025-03-26 21:19:12 -0600

    Page Transitions fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/TU_REPOSITORIO/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/gdg-ml-papers/" : "/",
   outDir: 'dist'
 });
